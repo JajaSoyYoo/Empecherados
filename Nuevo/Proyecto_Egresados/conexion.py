b632fd7db1f871d28b8f4c8b136e0b692e450c5b
JajaSoyYoo <alan.hcristobal@alumnos.udg.mx>
2023-09-27 13:11:09 -0600

    actualizacion importante

---
+++ Nuevo/Proyecto_Egresados/conexion.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def jalar():
+    return render_template('inicio.html')
+
+if __name__ == '__main__':
+    app.run(debug = True, port = 5000)
