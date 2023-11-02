d72f5b7e7a19dc73892917d391d60b4213f3ce19
JajaSoyYoo <128069293+JajaSoyYoo@users.noreply.github.com>
2023-11-01 20:56:36 -0600

    a

--- Nuevo/Proyecto_Egresados/control.py
+++ Nuevo/Proyecto_Egresados/control.py
@@ -17,10 +17,9 @@ def add_registro():
          horario= request.form['horariolaboral']
          puesto= request.form['puestolaboral']
          siOno = request.form['trabajasiono']
-         cursor = mysql.connection.cursor()
-         cursor.execute("insert into info_laboral (Trabajando, Direccion_trabajo, Horario_Laboral, Puesto_Trabajo, Correo_PK_Info) values ('"+siOno+"', '"+lugar+"', '"+horario+"', '"+puesto+"','alan.hcris@gmail.com')")
-         mysql.connection.commit()
-         return general()
+         #cursor = mysql.connection.cursor()
+         #cursor.execute("insert into info_laboral (Trabajando, Direccion_trabajo, Horario_Laboral, Puesto_Trabajo, Correo_PK_Info) values ('"+siOno+"', '"+lugar+"', '"+horario+"', '"+puesto+"','alan.hcris@gmail.com')")
+         #mysql.connection.commit()
 
 @app.route('/dashboard')
 def dashboard():
@@ -31,9 +30,42 @@ def dashboard():
     return render_template('dashboard.html', data=data)
 
 
-@app.route('/General')
+@app.route('/general')
 def general():
      return render_template('Generales.html')
+
+@app.route("/general_r", methods=['POST'])
+def registro_general():
+     if request.method == 'POST':
+          nombre_gen = request.form['nombres']
+          apellido_p = request.form['apellido_p']
+          apellido_m = request.form['apellido_m']
+          sexo = request.form['sexo']
+          telefono = request.form['tel_contacto']
+          correo = request.form['correo_alumno']
+          c_postal = request.form['codigo_postal']
+          pais = request.form['pais']
+          estado = request.form['estado']
+          ciudad = request.form['ciudad']
+          colonia = request.form['colonia']
+          nacionalidad = request.form['nacionalidad']
+          f_nacimiento = request.form['f_nacimiento']
+          return estudios()
+
+@app.route('/estudios')
+def estudios():
+     return render_template('Estudios.html')
+
+@app.route('/estudios_r', methods=['POST'])
+def estudios_r():
+     if request.method == 'POST':
+          nivel_estudios = request.form['nivel']
+          carrera = request.form['carrera']
+          titulado = request.form['titulado']
+          ciclo = request.form['ciclo']
+          ingles = request.form['ingles']
+          promedio = request.full_path['promedio']
+          return add_registro()
 
 
 @app.route('/inicio')
--- Nuevo/Proyecto_Egresados/model.py
+++ Nuevo/Proyecto_Egresados/model.py
@@ -2,7 +2,7 @@ class DevelopmentConfig():
     DEBUG = True
     MYSQL_HOST = 'localhost'
     MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'root'
+    MYSQL_PASSWORD = '1234'
     MYSQL_DB = 'seg_egresados2'
 
 
