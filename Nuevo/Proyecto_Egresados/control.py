a315979a86c8cf3c6d122856e1c13531f2ad03af
Kpy115 <alanmissaeljimenez@gmail.com>
2023-11-07 19:44:29 -0600

    LOGIN FUNCIONAL

--- Nuevo/Proyecto_Egresados/control.py
+++ Nuevo/Proyecto_Egresados/control.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_mysqldb import MySQL
+
 
 #from Nuevo.Proyecto_Egresados.control import model
 
 app = Flask(__name__)
+app.secret_key = 'tu_clave_secreta'
 mysql=MySQL(app)
 
 @app.route('/general', methods=['GET','POST'])
@@ -50,7 +52,11 @@ def add_registro():
 
 
 
-
+@app.before_request
+def before_request():
+    if 'user_id' not in session and request.endpoint != 'login':
+        flash('Debes iniciar sesión para acceder a esta página.', 'warning')
+        return redirect(url_for('login'))
 
 @app.route('/dashboard')
 def dashboard():
@@ -65,17 +71,32 @@ def dashboard():
 def inicio():
      return render_template('inicio.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-     return render_template('login.html')
+    if request.method == 'POST':
+        correo = request.form['correo']
+        contra = request.form['contra']
+
+        cursor = mysql.connection.cursor()
+        cursor.execute("SELECT * FROM cordis WHERE Correo_Cordinador = %s AND contra = %s", (correo, contra))
+        user = cursor.fetchone()
+        cursor.close()
+
+        if user:
+            # Autenticación exitosa, establecer una sesión para el usuario.
+            session['user_id'] = user[0]  # Accede al primer valor en la tupla (Correo_Cordinador)
+            return redirect(url_for('dashboard'))
+        else:
+            # Autenticación fallida
+            flash('Credenciales incorrectas. Por favor, inténtalo de nuevo.', 'danger')
+            return redirect(url_for('login'))
+
+    return render_template('login.html')
 
 @app.route('/seleccion')
 def seleccion():
      return render_template('seleccion.html')
 
-@app.route('/dash')
-def dash():
-     return render_template('dashboard.html')
 
 def Error404(error):
     return '<h1>Contacte a soporte tecnico</h1>'
